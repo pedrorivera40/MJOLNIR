5ab13852837f225461e3786656a2184649552c4d
Onix Tarrats Calderon <onix.tarrats@upr.edu>
2020-03-26 19:45:10 -0400

    Initial setup for firebase RTDB connection, NEED to update firebase app config.

---
+++ Loki/services/firebaseInit.js
@@ -0,0 +1,7 @@
+import firebase from 'firebase'
+import firebaseConfig from './firebaseConfig'
+
+
+/* Check if there is one firebase app already initialized, if not, intialize it. 
+Otherwise, use the curret app. This avoids errors with 'duplicate' firebase apps. */
+export default !firebase.apps.length ? firebase.initializeApp(firebaseConfig).database() : firebase.app().database()
