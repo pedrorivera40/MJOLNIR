008cee0a40940c8474454b3b3ca8bbd53b96b8e2
pedrorivera40 <pedro.rivera40@upr.edu>
2020-03-27 21:38:30 -0400

    setting up Firebase Admin SDK

--- OdinAPI/handler/dao/pbp_dao.py
+++ OdinAPI/handler/dao/pbp_dao.py
@@ -1,5 +1,20 @@
-from config.fb_config import fb_config
-from firebase import Firebase
+from config.fb_config import serv_path, rtdb_config
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
 
-rtdb = Firebase(fb_config).database()
-print(rtdb.child("v1").get().val())
+
+class PBPDao:
+
+    # Initialize Firebase Admin SDK and set Realtime Database instance.
+    def __init__(self):
+        firebase_admin.initialize_app(
+            credentials.Certificate(serv_path), rtdb_config)
+        self.rtdb = db
+
+
+if __name__ == '__main__':
+
+    # Initialize a Play-by-Play DAO for quick testing purposes.
+    pbp_dao = PBPDao()
+    print(pbp_dao.rtdb.reference("v1").get())
