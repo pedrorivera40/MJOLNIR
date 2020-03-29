a75457a76de429570a827bc4c9ed7c8cc2b1ed37
Luis <luis.ruelas@upr.edu>
2020-03-29 19:24:59 -0400

    Changed status codes on Position Handler class.

--- OdinAPI/handler/position.py
+++ OdinAPI/handler/position.py
@@ -18,7 +18,7 @@ class PositionHandler:
         mappedResult = []
         for position in result:
             mappedResult = self.mapPositionToDict(position)
-        return jsonify(Positions = mappedResult), 201
+        return jsonify(Positions = mappedResult), 200
     
     def getPositionByName(self,psName):
         dao = PositionDAO()
@@ -26,7 +26,7 @@ class PositionHandler:
         if not result:
             return jsonify(Error = "Position with the name:{} not found.".format(psName)), 404
         mappedResult = self.mapPositionToDict(result)
-        return jsonify(Position = mappedResult), 201
+        return jsonify(Position = mappedResult), 200
     
     def getPositionByAID(self,sID,aID):
         dao = PositionDAO()
@@ -34,7 +34,7 @@ class PositionHandler:
         if not result:
             return jsonify(Error = "Position not found  for the sport with id:{} and athlete with id:{} not found.".format(sID,aID)),404
         mappedResult = self.mapPositionToDict(result)
-        return jsonify(AthletePosition = mappedResult), 201
+        return jsonify(AthletePosition = mappedResult), 200
 
     def addPosition(self,sID,aID,psName):
         dao = PositionDAO()
@@ -50,14 +50,14 @@ class PositionHandler:
         if not result:
             return jsonify(Error = "Position to edit was not found."), 404
         mappedResult = self.mapPositionToDict(result)
-        return jsonify(Position = mappedResult), 201
+        return jsonify(Position = mappedResult), 200
     
     def removePosition(self,pID,sID,aID):
         dao = PositionDAO()
         result = dao.removePosition(pID,sID,aID)
         if not result:
             return jsonify(Error = "Position to remove was not found."), 404
-        return jsonify(Position = "Position for the athlete with id:{} was removed.".format(result)), 201
+        return jsonify(Position = "Position for the athlete with id:{} was removed.".format(result)), 200
 
         
     
