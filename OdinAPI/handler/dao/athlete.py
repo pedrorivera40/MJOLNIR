c8ef9ffe163baa83a764249b1026f314450d23cd
Luis <luis.ruelas@upr.edu>
2020-04-01 02:16:52 -0400

    Modified addAthlete() in AthleteDAO class.
    -Now when adding a new athlete their position is added to the athlete_position table if a position name is given for a valid sport that has said position.
    -Also when adding a new athlete their category is added to the athlete_position table if a category name is given for a valid sport that has said category.

--- OdinAPI/handler/dao/athlete.py
+++ OdinAPI/handler/dao/athlete.py
@@ -13,16 +13,16 @@ class AthleteDAO:
         self.conn = psycopg2.connect(connection_url)
 
 
-    def getAthletesBySport(self,sID,aBranch):
+    def getAthletesBySport(self,sID):
         cursor = self.conn.cursor()
-        query = "select A.id,A.first_name,A.middle_name,A.last_names,A.short_bio,A.height_inches,A.study_program,A.date_of_birth,A.school_of_precedence,A.number,A.profile_image_link "\
-                "from (athlete as A inner join (team_members as TM inner join team as T on TM.team_id = T.id) on A.id = TM.athlete_id)"\
-                "where T.sport_id = %s "\
-                "and T.branch_id = %s"
-        cursor.execute(query,(sID,aBranch,))        
+        query = """select A.id,A.first_name,A.middle_name,A.last_names,A.short_bio,A.height_inches,A.study_program,A.date_of_birth,A.school_of_precedence,A.number,A.profile_image_link,A.is_invalid,S.name as Sport_name,P.name as position_name,C.name as category_name
+                   from ((athlete as A inner join sport as S on A.sport_id=S.id) full outer join (athlete_position as AP inner join position as P on AP.position_id=P.id) on AP.athlete_id=A.id) full outer join (athlete_category as AC inner join category as C on AC.category_id=C.id) on A.id=AC.athlete_id
+                   where S.id = %s
+                   and A.is_invalid=false
+                """
+        cursor.execute(query,(sID,))        
         result = []
-        for row in cursor:
-            #print(row)
+        for row in cursor:            
             result.append(row)
         return result       
     
@@ -56,14 +56,57 @@ class AthleteDAO:
             result.append(row)
         return result
 
-    def addAthlete(self,sID,aFName, aMName, aLName, aBio, aHeight,aStudyProgram,aDateOfBirth, aSchoolOfPrecedence,aNumber,aProfilePictureLink):
-        cursor = self.conn.cursor()
+    def addAthlete(self,sID,aFName, aMName, aLName, aBio, aHeight,aStudyProgram,aDateOfBirth, aSchoolOfPrecedence,aNumber,aProfilePictureLink,aPosition,aCategory):
+        cursor = self.conn.cursor()                
         query = "insert into athlete(first_name,middle_name,last_names,short_bio,height_inches,study_program,date_of_birth,school_of_precedence,number,profile_image_link,sport_id,is_invalid) "\
                 "values (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,'false') returning id;"
         cursor.execute(query,(aFName, aMName, aLName, aBio, aHeight,aStudyProgram,aDateOfBirth, aSchoolOfPrecedence,aNumber,aProfilePictureLink,sID,))
+        
+        
         aID = cursor.fetchone()[0]
         if not aID:
-            return aID
+            return []#Empty
+        
+        
+        if aPosition != None:
+            query = """select P.id
+                       from position as P inner join sport as S on P.sport_id=S.id
+                       where S.id = %s
+                       and P.name = %s                    
+                    """
+            cursor.execute(query,(sID,aPosition,))
+            pID = cursor.fetchone()
+            print(pID)
+            if not pID:
+                return []
+            
+            print(pID)
+            query = """insert into athlete_position(position_id,athlete_id,is_invalid)
+                       values(%s,%s,'false') returning id
+                    """
+            cursor.execute(query,(pID,aID,))
+            apID = cursor.fetchone()[0]
+            if not apID:
+                return []
+        if aCategory != None:
+            query = """select C.id
+                       from  category as C inner join sport as S on C.sport_id=S.id
+                       where S.id = %s
+                       and C.name = %s
+                    """
+            cursor.execute(query,(sID,aCategory,))
+            cID = cursor.fetchone()
+            if not cID:
+                return []
+            
+            query = """insert into athlete_category(athlete_id,category_id,is_invalid)
+                       values(%s,%s,'false') returning id
+                    """
+            cursor.execute(query,(aID,cID,))
+            acID = cursor.fetchone()[0]
+            if not acID:
+                return []
+
         self.commitChanges()
         return aID
 
