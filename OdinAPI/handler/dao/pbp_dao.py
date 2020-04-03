7bb724a586c8901bca30b91a8409a5f8403d7029
pedrorivera40 <pedro.rivera40@upr.edu>
2020-04-03 02:02:57 -0400

    updating PBP handler + init docs

--- OdinAPI/handler/dao/pbp_dao.py
+++ OdinAPI/handler/dao/pbp_dao.py
@@ -1,11 +1,17 @@
-from config.fb_config import serv_path, rtdb_config
+from .config.fb_config import serv_path, rtdb_config
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import db
 
 
 # TODO -> Add class documentation
-class VolleyballPBPDao:
+class PBPDao:
+    '''
+    PBPDao - This class implements a Data Access Object pattern to provide 
+             Odin API interaction to information about sport play-by-play 
+             sequences stored in the non-relational database.
+    @author Pedro Luis Rivera Gomez
+    '''
 
     # Initialize Firebase Admin SDK and set Realtime Database instance.
     def __init__(self):
@@ -22,33 +28,48 @@ class VolleyballPBPDao:
             "score": "/score",
             "actions": "/game-actions",
             "score-key": "score",
-            "score-val": {
-                "set1-opponent": 0,
-                "set1-uprm": 0,
-                "set2-opponent": 0,
-                "set2-uprm": 0,
-                "set3-opponent": 0,
-                "set3-uprm": 0,
-                "set4-opponent": 0,
-                "set4-uprm": 0,
-                "set5-opponent": 0,
-                "set5-uprm": 0
-            },
+            "over": "/game-ended",
+            "answer": "/answer",
             "uprm-sets": ["/set1-uprm", "/set2-uprm", "/set3-uprm", "/set4-uprm", "/set5-uprm"],
             "opp-sets": ["/set1-opponent", "/set2-opponent", "/set3-opponent", "/set4-opponent", "/set5-opponent"]
         }
 
-    def create_volleyball_pbp_seq(self, event_id, game_metadata, uprm_roster, opponent_roster):
-        # Define basic structure of a PBP node contained in the NoSQL database.
+    def create_pbp_seq(self, event_id, game_metadata, score_val):
+        """
+        Initialize a PBP sequence for a valid sport event.
+        This function creates a PBP sequence initializing its metadata and scoring system.
+
+        Args
+            event_id: integer corresponding to an event id.
+            game_metadata: JSON object containing general information about a PBP sequence depending on the sport.
+            score_val: JSON object containing the initial scores per period of a PBP sequence depending on the sport.
+
+        Returns:
+            void
+        """
+
         event_node = {
             self._db_keywords["meta"]: game_metadata,
-            self._db_keywords["score-key"]: self._db_keywords["score-val"]
+            self._db_keywords["score-key"]: score_val
         }
 
-        return self._rtdb.reference(self._db_keywords["root"] + event_id).set(event_node)
+        path = self._db_keywords["root"] + event_id
+        self._rtdb.reference(path).set(event_node)
 
-    def remove_volleyball_pbp_seq(self, event_id):
-        return self._rtdb.reference(self._db_keywords["root"] + event_id).delete()
+    def remove_pbp_seq(self, event_id):
+        """
+        Remove a PBP sequence from the non-relational database.
+        This function deletes a PBP sequence's node from the non-relational database.
+
+        Args
+            event_id: integer corresponding to an event id.
+
+        Returns:
+            void
+        """
+
+        path = self._db_keywords["root"] + event_id
+        return self._rtdb.reference(path).delete()
 
     def set_pbp_metadata(self, event_id, metadata):
         return self._rtdb.reference(self._db_keywords["root"] + event_id + self._db_keywords["meta"]).set(metadata)
@@ -80,6 +101,9 @@ class VolleyballPBPDao:
     def get_opponent_score(self, event_id, set):
         return self._rtdb.reference(self._db_keywords["root"] + event_id + self._db_keywords["score"] + self._db_keywords["opp-sets"][set - 1]).get()
 
+    def pbp_game_action_exists(self, event_id, action_id):
+        return self._rtdb.reference(self._db_keywords["root"] + event_id + self._db_keywords["actions"] + "/" + action_id).get() != None
+
     def add_pbp_game_action(self, event_id, action_content):
         return self._rtdb.reference(self._db_keywords["root"] + event_id + self._db_keywords["actions"]).push(action_content)
 
@@ -87,19 +111,63 @@ class VolleyballPBPDao:
         return self._rtdb.reference(self._db_keywords["root"] + event_id + self._db_keywords["actions"] + "/" + action_id).set(action_content)
 
     def remove_pbp_game_action(self, event_id, action_id):
-        return self._rtdb.reference(self._db_keywords["root"] + event_id + self._db_keywords["actions"] + "/" + action_id).delete()
+        """
+        Removes a game action from PBP sequence.
+        This function deletes a particular game action from PBP sequence.
+
+        Args
+            event_id: integer corresponding to an event id.
+
+        Returns:
+            void
+        """
+
+        path = self._db_keywords["root"] + int(event_id) + \
+            self._db_keywords["actions"] + "/" + int(action_id)
+
+        self._rtdb.reference(path).delete()
+
+    def is_game_over(self, event_id):
+        """
+        Determines if a particular game is over.
+        This function returns the game-ended status of a PBP sequence (Boolean).
+
+        Args
+            event_id: integer corresponding to an event id.
+
+        Returns:
+            Boolean result that determines if the game is over or not.
+        """
+
+        path = self._db_keywords["root"] + int(event_id) + \
+            self._db_keywords["meta"] + \
+            self._db_keywords["over"] + self._db_keywords["answer"]
+
+        return self._rtdb.reference(path).get()
+
+    def set_pbp_game_over(self, event_id):
+        """
+        Marks a PBP sequence state as over.
+        This function modifies the game-ended status of a PBP sequence to true.
+
+        Args
+            event_id: integer corresponding to an event id.
+
+        Returns:
+            void
+        """
+
+        path = self._db_keywords["root"] + int(event_id) + \
+            self._db_keywords["meta"] + self._db_keywords["over"]
+
+        self._rtdb.reference(path).set({"answer": True})
 
 
-if __name__ == '__main__':
+# if __name__ == '__main__':
 
-    # Initialize a Play-by-Play DAO for quick testing purposes.
-    pbp_dao = VolleyballPBPDao()
-    print(pbp_dao._rtdb.reference("v1").get())
-    print(pbp_dao.pbp_exists("crap"))
-    print(pbp_dao._rtdb.reference("v1/crap").delete())
-    print(pbp_dao.pbp_exists("crap"))
-    # print(pbp_dao.get_current_set("unique-volleyball-game-id-1"))
-    # pbp_dao.create_volleyball_pbp_seq(1, 1, 1, 1)
-    # print(pbp_dao._rtdb.reference("/v1/caca").push({"a": "1"}))
-    # print(pbp_dao._rtdb.reference(
-    #     "/v1/caca/-M3iczDv5Lii5kzBT8Ms").update({"a": "3"}))
+#     # Initialize a Play-by-Play DAO for quick testing purposes.
+#     pbp_dao = PBPDao()
+#     print(pbp_dao._rtdb.reference("v1").get())
+#     print(pbp_dao.pbp_exists("crap"))
+#     print(pbp_dao._rtdb.reference("v1/crap").delete())
+#     print(pbp_dao.pbp_exists("crap"))
--- OdinAPI/handler/pbp_handler.py
+++ OdinAPI/handler/pbp_handler.py
@@ -1,22 +1,213 @@
-from dao.pbp_dao import VolleyballPBPDAO
+from flask import jsonify
+from .dao.pbp_dao import PBPDao as VolleyballPBPDao
+from.dao.mock.event_dao import _mockEventDAO as EventDAO
 
 
 class PBPHandler:
+    '''
+    PBPHandler - This class handles incomming requests from Odin API's gateway
+                 by interacting with the PBPDAO. It is responsible for modifying
+                 the state of content stored in the non-relational database.
+    @author Pedro Luis Rivera Gomez
+    '''
 
     def __init__(self):
-        self.dao = VolleyballPBPDAO()
+        """
+        Default constructor - initialize keywords that apply to Volleyball PBP sequences.
+        """
 
-    def startPBPSequence(self, event_id, sequence_info):
-        # THIS IS JUST TO REMIND MYSELF I HAVE TO TRY/CATCH INTERACTION WITH DAO.
-        # try:
-        #     self.dao.startPBPSequence
-        # except:
-        #      return
+        self._sport_keywords = {
+            "score-val": {
+                "set1-opponent": 0,
+                "set1-uprm": 0,
+                "set2-opponent": 0,
+                "set2-uprm": 0,
+                "set3-opponent": 0,
+                "set3-uprm": 0,
+                "set4-opponent": 0,
+                "set4-uprm": 0,
+                "set5-opponent": 0,
+                "set5-uprm": 0
+            },
+            "sport": "Voleibol"
+        }
+
+    def startPBPSequence(self, event_id):
+        """
+        Starts a PBP sequence.
+        This function interacts with the PBP DAO to create a new PBP sequence.
+
+        Args
+            event_id: integer corresponding to an event id.
+
+        Returns:
+            Response containing a MSG in case of success, or ERROR message in case of failure.
+        """
+
+        try:
+            event_info = EventDAO().getEventById(event_id)
+
+            if not event_info:
+                return jsonify(ERROR="PBPHandler.startPBPSequence: Invalid event."), 400
+
+            if len(event_info) != 7:
+                return jsonify(ERROR="PBPHandler.startPBPSequence: Invalid response from Event DAO."), 500
+
+            if event_info["sport_name"] != self._sport_keywords["sport"]:
+                return jsonify(ERROR="PBPHandler.startPBPSequence: Sport does not match Volleyball."), 403
+
+        except:
+            return jsonify(ERROR="PBPHandler.startPBPSequence: Could not retrieve information from Event DAO."), 500
+
+        try:
+            pbp_dao = VolleyballPBPDao()
+            if pbp_dao.pbp_exists(event_id):
+                return jsonify(ERROR="PBPHandler.startPBPSequence: PBP Sequence already created."), 403
+
+            # At this point, the event exists and does not have a PBP sequence.
+            game_metadata = {
+                "branch": event_info[0],
+                "game-ended": {"answer": "No"},
+                "location": "Home" if event_info[1] else "Away",
+                "opponent-color": event_info[2],
+                "opponent-name": event_info[3],
+                "sport": event_info[4],
+                "venue": event_info[5]
+            }
+
+            pbp_dao.create_volleyball_pbp_seq(
+                event_id, game_metadata, self._sport_keyword["score-val"])
+
+            return jsonify(CREATED="PBP Sequence for " + id + " was successfully created"), 200
+
+        except:
+            return jsonify(ERROR="PBPHandler.startPBPSequence: Could not retrieve information from PBP DAO."), 500
+
+    def addUPRMRoster(self):
+        return 1
+
+    def addOppRoster(self):
+        return 1
+
+    def editUPRMRoster(self):
+        return 1
+
+    def editOppRoster(self):
+        return 1
+
+    def removeUPRMRoster(self):
+        return 1
+
+    def removeOppRoster(self):
+        return 1
+
+    def addPBPAction(self, event_id, action_data):
+        """
+        Adds a PBP game action into the feed.
+        This function interacts with the PBP DAO to insert a Volleyball game action.
+
+        Args
+            event_id: integer corresponding to an event id.
+            action_data: JSON object containing the new game action's value.
+
+        Returns:
+            Response containing a MSG in case of success, or ERROR message in case of failure.
+        """
+
+        try:
+            event_dao = EventDAO()
+
+            # TODO -> check if it would be better adding another method in the DAO for getting sportByEventId.
+            if event_dao.getEventById(event_id)[4] != self._sport_keywords["sport"]:
+                return jsonify(MSG="PBPHandler.setPBPSequenceOver: Not a volleyball event."), 403
+
+            pbp_dao = VolleyballPBPDao()
+            if pbp_dao.pbp_exists(event_id):
+
+                if pbp_dao.is_game_over(event_id):
+                    return jsonify(ERROR="PBPHandler.addPBPAction: PBP sequence already over."), 403
+                # TODO -> validate sequence info is complete... VALIDATE SPORT == voleibol...
+                return jsonify(ADDED=pbp_dao.add_pbp_game_action(event_id, {})), 200
+
+            return jsonify(ERROR="PBPHandler.addPBPAction: Non-existing PBP sequence."), 403
+
+        except:
+            return jsonify(ERROR="PBPHandler.addPBPSequence: Could not retrieve information from PBP DAO."), 500
+
+    def editPBPAction(self, event_id, action_id, new_action):
+        """
+        Edits a PBP game action from the feed.
+        This function interacts with the PBP DAO to edit a Volleyball game action
+        by replacing its previous value with a new one.
+
+        Args
+            event_id: integer corresponding to an event id.
+            action_id: string corresponding to a game action id.
+            new_action: JSON object containing the new game action's value.
+
+        Returns:
+            Response containing a MSG in case of success, or ERROR message in case of failure.
+        """
 
         return 0
 
-    def editPBPSequence(self, event_id, sequence_change):
-        return 0
+    def removePlayPBPAction(self, event_id, game_action_id):
+        """
+        Removes a PBP game action from the feed.
+        This function interacts with the PBP DAO to remove a Volleyball game action.
 
-    def removePlayPBPSequence(self, event_id, game_action_id):
-        return 0
+        Args
+            event_id: integer corresponding to an event id.
+
+        Returns:
+            Response containing a MSG in case of success, or ERROR message in case of failure.
+        """
+
+        try:
+            event_dao = EventDAO()
+
+            # TODO -> check if it would be better adding another method in the DAO for getting sportByEventId.
+            if event_dao.getEventById(event_id)[4] != self._sport_keywords["sport"]:
+                return jsonify(MSG="PBPHandler.setPBPSequenceOver: Not a volleyball event."), 403
+
+            pbp_dao = VolleyballPBPDao()
+            if pbp_dao.pbp_exists(event_id):
+
+                if pbp_dao.pbp_game_action_exists(event_id, game_action_id):
+                    return jsonify(pbp_dao.remove_pbp_game_action(event_id, game_action_id)), 200
+
+                return jsonify(ERROR="PBPHandler.setPBPSequenceOver: Non-existing game action."), 403
+
+            return jsonify(ERROR="PBPHandler.setPBPSequenceOver: Non-existing PBP Sequence."), 403
+        except:
+            return jsonify(ERROR="PBPHandler.removePlayPBPSequence: Internal error from PBP DAO."), 500
+
+    def setPBPSequenceOver(self, event_id):
+        """
+        Marks a PBP sequence state as over.
+        This function interacts with the PBP DAO to modify the game-ended status of a Volleyball
+        PBP sequence to true.
+
+        Args
+            event_id: integer corresponding to an event id.
+
+        Returns:
+            Response containing a MSG in case of success, or ERROR message in case of failure.
+        """
+
+        try:
+            pbp_dao = VolleyballPBPDao()
+            event_dao = EventDAO()
+
+            # TODO -> check if it would be better adding another method in the DAO for getting sportByEventId.
+            if event_dao.getEventById(event_id)[4] != self._sport_keywords["sport"]:
+                return jsonify(MSG="PBPHandler.setPBPSequenceOver: Not a volleyball event."), 403
+
+            if pbp_dao.pbp_exists(event_id):
+                pbp_dao.set_pbp_game_over(event_id)
+                return jsonify(MSG="PBPHandler.setPBPSequenceOver: Success."), 200
+
+            return jsonify(ERROR="PBPHandler.setPBPSequenceOver: Non-existing PBP Sequence."), 403
+
+        except:
+            return jsonify(ERROR="PBPHandler.setPBPSequenceOver: Internal error from PBP DAO."), 500
