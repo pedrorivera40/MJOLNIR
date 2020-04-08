d7e8c48bc8912074d90999b4574d4e5d058ec708
Onix Tarrats Calderon <onix.tarrats@upr.edu>
2020-04-08 01:04:41 -0400

    Update new user to run a test suite.

--- OdinAPI/tests/newUserData.py
+++ OdinAPI/tests/newUserData.py
@@ -1,10 +1,10 @@
 # TODO: Edit this user object before starting new test.
 newUser = {
-        'username': 'newUser26',
-        'email': 'newuser26@email.com',
-        'full_name': 'New User26',
+        'username': 'newUser27',
+        'email': 'newuser27@email.com',
+        'full_name': 'New User27',
         'password':'N3wP4ssword!',
       }
 
-newUserID = 77 #TODO: Check database before running tests to ensure the correct number is used.
+newUserID = 79 #TODO: Check database before running tests to ensure the correct number is used.
 # TODO: Also change the newnew user on the test of actions on removed user.
