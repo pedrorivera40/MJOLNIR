50a241699f214b3029fa4eb54c2265def8190e83
Onix Tarrats Calderon <onix.tarrats@upr.edu>
2020-04-07 23:01:30 -0400

    Added case in test 9 for inexistent user, ermpty permission list and invalid request format.

--- OdinAPI/tests/test_9_user_set_permissions.py
+++ OdinAPI/tests/test_9_user_set_permissions.py
@@ -89,4 +89,80 @@ class TestUserRoutes(unittest.TestCase):
             else:
                 self.assertEqual(permission['is_invalid'], False)
 
+    def test_set_user_permissions_inexistent_user(self):
+        response = self.client.patch(f'/users/8923892387/permissions', data=json.dumps(self.default_permissions), content_type='application/json',  follow_redirects=True)
+        self.assertEqual(response.status_code, 404)
+        self.assertEqual(response.json['Error'], 'No User found in the system with that id.')
+
+
+    def test_set_user_permissions_empty_permissions(self):
+        response = self.client.patch(f'/users/{newUserID}/permissions', data=json.dumps({'permissions': None}), content_type='application/json',  follow_redirects=True)
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.json['Error'], 'Permissions cant be empty.')
+
+    def test_set_user_permissions_invalid_format(self):
+        response = self.client.patch(f'/users/{newUserID}/permissions', data=json.dumps({"permissions": [
+                {
+                    "is_inlid": True,
+                    "permison_id": 13
+                },
+                {
+                    "is_invalid": False,
+                    "permission_id": 14
+                },
+                {
+                    "is_invalid": False,
+                    "permission_id": 15
+                },
+                {
+                    "is_invalid": True,
+                    "permission_id": 16
+                },
+                {
+                    "is_invalid": False,
+                    "permission_id": 17
+                },
+                {
+                    "is_invalid": False,
+                    "permission_id": 18
+                },
+                {
+                    "is_invalid": False,
+                    "permission_id": 19
+                },
+                {
+                    "is_invalid": False,
+                    "permission_id": 20
+                },
+                {
+                    "is_invalid": False,
+                    "permission_id": 21
+                },
+                {
+                    "is_invalid": False,
+                    "permission_id": 22
+                },
+                {
+                    "is_invalid": False,
+                    "permission_id": 23
+                },
+                {
+                    "is_invalid": False,
+                    "permission_id": 24
+                },
+                {
+                    "is_invalid": False,
+                    "permission_id": 25
+                },
+                {
+                    "is_invalid": False,
+                    "permission_id": 26
+                },
+                {
+                    "is_invalid": True,
+                    "permission_id": 27
+                }]}), content_type='application/json',  follow_redirects=True)
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.json['Error'], 'Bad Request')
+
 
