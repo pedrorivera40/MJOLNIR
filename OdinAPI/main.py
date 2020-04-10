e8ff72dce6a5e2fac016cf4c03b87ef429da9bfb
Onix Tarrats Calderon <onix.tarrats@upr.edu>
2020-04-09 23:42:55 -0400

    Added logged in user session in the api. And helper methods to get and drop the session

--- OdinAPI/main.py
+++ OdinAPI/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, session
 
 from flask_cors import CORS
 import os
@@ -26,6 +26,8 @@ load_dotenv()
 
 
 app = Flask(__name__)
+app.secret_key = 'any random string'
+
 CORS(app)
 
 
@@ -117,8 +119,21 @@ def auth():
             
         username = req['username']
         password = req['password'] # TODO: AES Encryption
-        
         return handler.login(username, password)
+@app.route("/auth/user", methods=['GET'])
+def userSession():
+    if request.method == 'GET':
+        if 'username' in session:
+            return jsonify(User=session['username'])
+        return jsonify(Error='Could not get session')
+
+@app.route("/auth/drop", methods=['GET'])
+def dropSession():
+    if request.method == 'GET':
+        if 'username' in session:
+            session.pop('username', None)
+            return jsonify(Message='Session droped.')
+        return jsonify(Error='Could not drop session')
         
 
 ###########################################
