b06f3882895ec330a0c996bc7aab73bb70888149
Onix Tarrats Calderon <onix.tarrats@upr.edu>
2020-04-11 10:17:00 -0400

    Created the snackbar componet and added it to the layouts.

---
+++ LokiDashboard/components/TheSnackBar.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <v-snackbar
+      v-for="(snackbar, index) in snackbars.filter(s => s.showing)"
+      :key="snackbar.text + Math.random()"
+      v-model="snackbar.showing"
+      :timeout="snackbar.timeout"
+      :color="snackbar.color"
+      :style="`bottom: ${(index * 60) + 8}px`"
+    >
+      {{snackbar.text}}
+      <v-btn text @click="snackbar.showing = false">Close</v-btn>
+    </v-snackbar>
+  </div>
+</template>
+
+<script>
+  import { mapGetters } from 'vuex';
+  export default {
+    name: 'TheSnackbar',
+    computed: {
+      ...mapGetters({
+        snackbars: 'notifications/snackbars'
+      })
+    },    
+  }
+</script>
