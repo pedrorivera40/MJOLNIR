38c2d3e82f72c01a0d7eb6bccf33dd4cb68d76fe
Onix Tarrats Calderon <onix.tarrats@upr.edu>
2020-04-11 11:17:26 -0400

    Reset password is working

--- LokiDashboard/store/userAuth/actions.js
+++ LokiDashboard/store/userAuth/actions.js
@@ -8,7 +8,7 @@ export default {
       commit("SET_USER_DATA", response.data.User) 
       dispatch('notifications/setSnackbar', {text: 'Login Sucessfully'}, {root: true})
     } catch (error) {
-      dispatch('notifications/setSnackbar', {text: error.response.data.Error, color: 'error'}, {root: true})
+      // dispatch('notifications/setSnackbar', {text: error.response.data.Error, color: 'error'}, {root: true})
       commit("DONE_LOADING")
     }
   },
@@ -19,10 +19,19 @@ export default {
     commit("SET_USER_DATA_ON_RELOAD")
   },
   async reset({commit}, credentials){
-    const user = await this.$axios.post('users/username/', {username: credentials.username}) //call get user by username to set auth user.
-    const id = user.data.User.id
+    let id = ''
+    try {
+      const user = await this.$axios.post('users/username/', {username: credentials.username}) //call get user by username to set auth user.
+      id = user.data.User.id
     
-    console.log()
+    } catch (error) {
+      console.log(error.response.data.Error)
+    }
+    try {
+      await this.$axios.patch(`users/${id}/reset`, {username: credentials.username, password: credentials.password}) //call get user by username to set auth user.
+    } catch (error) {
+      console.log(error.response.data.Error)
+    }
   },
   logout({ commit }) {
     commit("CLEAR_USER_DATA")
