019f6ae49dba834c41e7444f2fdc13d4603b1de7
pedrorivera40 <pedro.rivera40@upr.edu>
2020-04-14 01:09:29 -0400

    adding sports to Loki

---
+++ LokiDashboard/components/SportCard.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-hover v-slot:default="{ hover }" open-delay="25">
+    <v-card class="mx-auto" max-width="400" :elevation="hover ? 16 : 2">
+      <v-img class="white--text align-end" height="200px" :src="img">
+        <template v-slot:placeholder>
+          <v-layout fill-height align-center justify-center ma-0>
+            <v-progress-circular indeterminate color="grey"></v-progress-circular>
+          </v-layout>
+        </template>
+      </v-img>
+      <v-card-title class="sport-span" hover="true">{{sport_name}}</v-card-title>
+    </v-card>
+  </v-hover>
+</template>
+
+<script>
+export default {
+  props: {
+    sport_name: String,
+    img: String
+  }
+};
+</script>
+
+<style>
+.sport-span {
+  background-color: rgb(67, 160, 71);
+  color: white;
+  font-weight: bold;
+  margin-right: 0;
+}
+</style>
---
+++ LokiDashboard/components/SportsView.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-row>
+    <v-col v-for="sport in sports" :key="sport.sport_id" cols="12" md="6" lg="3">
+      <SportCard
+        :sport_name="sport.sport_name"
+        :img="sport.sport_image_url"
+        @click.native="redirect(sport.sport_id)"
+      />
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+import SportCard from "../components/SportCard.vue";
+
+export default {
+  components: {
+    SportCard
+  },
+  props: {
+    sports: []
+  },
+  methods: {
+    redirect(sport_id) {
+      // This part integrates with Herbert's Team Page.
+      this.$router.push("equipo/" + sport_id);
+    }
+  }
+};
+</script>
