721d4c1e7007d361dc006ce0fc0a248c65536426
Onix Tarrats Calderon <onix.tarrats@upr.edu>
2020-04-15 18:20:22 -0400

    Added class hierarchy diagrams

---
+++ OdinAPI/docs/html/classtests_1_1test__12__remove__user__route_1_1_test_user_routes.js
@@ -0,0 +1,7 @@
+var classtests_1_1test__12__remove__user__route_1_1_test_user_routes =
+[
+    [ "setUp", "classtests_1_1test__12__remove__user__route_1_1_test_user_routes.html#acc5850df5f127b456421645d163d651b", null ],
+    [ "test_remove_user", "classtests_1_1test__12__remove__user__route_1_1_test_user_routes.html#a5231432a4fe390ec295e86878dda9504", null ],
+    [ "client", "classtests_1_1test__12__remove__user__route_1_1_test_user_routes.html#af5e6a4f409e4a00562e66876c3f8a459", null ],
+    [ "data", "classtests_1_1test__12__remove__user__route_1_1_test_user_routes.html#a50ad0e8b71fbace7abfca6526345acef", null ]
+];
---
+++ OdinAPI/docs/html/classtests_1_1test__13__actions__on__removed__user_1_1_test_user_routes.js
@@ -0,0 +1,11 @@
+var classtests_1_1test__13__actions__on__removed__user_1_1_test_user_routes =
+[
+    [ "setUp", "classtests_1_1test__13__actions__on__removed__user_1_1_test_user_routes.html#a0452644bd32ddb13fd0a2cc1840ab5d0", null ],
+    [ "test_add_new_user_with_username_of_removed_user", "classtests_1_1test__13__actions__on__removed__user_1_1_test_user_routes.html#a46d3cffc09c23e5928f59152c0b013e9", null ],
+    [ "test_get_user_by_id_of_removed_user", "classtests_1_1test__13__actions__on__removed__user_1_1_test_user_routes.html#ab9e4aee139ec8ad6146daca42332b062", null ],
+    [ "test_get_user_by_username_of_removed_user", "classtests_1_1test__13__actions__on__removed__user_1_1_test_user_routes.html#afc8631ce10ede4b6ff2120d03662fd89", null ],
+    [ "test_remove_removed_user", "classtests_1_1test__13__actions__on__removed__user_1_1_test_user_routes.html#a541d374f819e00df9fe83d205b50a37e", null ],
+    [ "test_toggle_active_on_removed_user", "classtests_1_1test__13__actions__on__removed__user_1_1_test_user_routes.html#a1beb450a43bb0e60bf70296789eb0dbe", null ],
+    [ "client", "classtests_1_1test__13__actions__on__removed__user_1_1_test_user_routes.html#a18d3632ffd5596b3a1f7af60494dba91", null ],
+    [ "data", "classtests_1_1test__13__actions__on__removed__user_1_1_test_user_routes.html#aeefb5f93b8f194f5c8695a8a8c77769a", null ]
+];
