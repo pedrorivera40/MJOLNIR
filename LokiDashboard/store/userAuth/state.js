c27433ba490eacef8b96a3c73fd43f3329470f16
Onix Tarrats Calderon <onix.tarrats@upr.edu>
2020-04-15 01:27:11 -0400

    documented authUser mutations

--- LokiDashboard/store/userAuth/state.js
+++ LokiDashboard/store/userAuth/state.js
@@ -1,5 +1,16 @@
 export default ()=>({
+  /**
+   * Loged in user object.
+   */
   user: null, //represents logged in user.
+
+  /**
+   * Logged in user permisions.
+   */
   userPermissions: [],
+
+  /**
+   * Loading state for auth  pages.
+   */
   isLoading: false
 })
