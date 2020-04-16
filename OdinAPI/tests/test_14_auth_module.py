992d688491e26ea55a6a154945d33629bd794445
Onix Tarrats Calderon <onix.tarrats@upr.edu>
2020-04-16 19:41:54 -0400

    finished tests for auth module

--- OdinAPI/tests/test_14_auth_module.py
+++ OdinAPI/tests/test_14_auth_module.py
@@ -37,3 +37,15 @@ class TestUserRoutes(unittest.TestCase):
   def test_rules_match_password_no_special_characters(self):
       result = auth.rulesMatch('newPasword11')
       self.assertEqual(result, False)
+
+  def test_password_hashing(self):
+    password = 'secretPasswordHehe'
+    hash = auth.createHash(password)
+    self.assertEqual(auth.verifyHash(password,hash), True)
+
+  def test_jwt_token_creation_and_verification(self):
+    username = 'elCanserbero'
+    permisions = {'permissions': 'somePermissions'}
+    token = auth.generateToken(username,permisions)
+    token = token.split(' ')[1]
+    self.assertEqual(auth.verifyToken(token), True)
