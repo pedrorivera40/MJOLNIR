7011b862fb6226059d040e1607f1e3a49e5ef6d4
herbert-perez <herbert.perez@upr.edu>
2020-04-20 12:44:27 -0400

    rebooting master from scratch in order to merge with dev

--- Freya/services/firebaseInit.js
+++
@@ -1,7 +0,0 @@
-import firebase from 'firebase'
-import firebaseConfig from './firebaseConfig'
-
-
-/* Check if there is one firebase app already initialized, if not, intialize it. 
-Otherwise, use the curret app. This avoids errors with 'duplicate' firebase apps. */
-export default !firebase.apps.length ? firebase.initializeApp(firebaseConfig).database() : firebase.app().database()
--- OdinAPI/handler/dao/pbp_dao.py
+++
@@ -1,5 +0,0 @@
-from config.fb_config import fb_config
-from firebase import Firebase
-
-rtdb = Firebase(fb_config).database()
-print(rtdb.child("v1").get().val())
