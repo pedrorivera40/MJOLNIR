87a2b16c2c7050295122385b1ad6c79dc3b7d6c1
pedrorivera40 <pedro.rivera40@upr.edu>
2020-04-20 21:18:00 -0400

    finish sport tests

--- OdinAPI/tests/sport_tests/test_get_sport_details.py
+++ OdinAPI/tests/sport_tests/test_get_sport_details.py
@@ -9,6 +9,7 @@ class TestGetAllSports(unittest.TestCase):
         app.config['DEBUG'] = True
         self.client = app.test_client()
 
-    def test_get_all_sports_good(self):
+    def test_get_sports_details_good(self):
         response = self.client.get('/sports/details',data=json.dumps({}),content_type='application/json', follow_redirects=True)
         self.assertEqual(response.status_code, 200)
+        print(response.json)
