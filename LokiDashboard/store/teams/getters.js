34f80fc380fcce3ae87c5034c9e08647390236bf
herbert-perez <herbert.perez@upr.edu>
2020-04-26 01:52:23 -0400

    small changes to freyawebsite store in order to make it independent from previous versions, and added store files to LokiDashboard

---
+++ LokiDashboard/store/teams/getters.js
@@ -0,0 +1,12 @@
+export default{
+    team: state => state.team,
+    teams: state => state.teams,
+    team_members: state => state.team_members,
+    team_member: state => state.team_member,
+    member_statistics: state => state.member_statistics,
+    team_statistics: state => state.team_statistics,
+    isLoadingT: state => state.isLoadingT,
+    readyForMembers: state => state.readyForMembers,
+    readyForMemberStats: state => state.readyForMemberStats,
+    readyForTeamStats: state => state.readyForTeamStats
+}
---
+++ LokiDashboard/store/teams/state.js
@@ -0,0 +1,16 @@
+export default() =>({
+    team: null,
+    teams: null,
+    team_members: null,
+    team_member:null,
+    member_statistics:null,
+    team_statistics:null,
+    isLoadingT: false,
+    readyForMembers: false,
+    readyForMemberStats: false,
+    readyForTeamStats: false,
+
+    // athlete: null,
+    // athletes: [],
+    // isLoadingA: false,
+})
