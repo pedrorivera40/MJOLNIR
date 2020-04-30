bb7be207267bf521edd0ccae0f852dd73cbb3dd7
pedrorivera40 <pedro.rivera40@upr.edu>
2020-04-30 15:43:07 -0400

    tested pbp roster remove using route arguments

--- OdinAPI/tests/pbp_tests/uprm_roster/test_uprm_roster_remove_valid.py
+++ OdinAPI/tests/pbp_tests/uprm_roster/test_uprm_roster_remove_valid.py
@@ -12,8 +12,8 @@ class TestRemoveUPRMRosterVolleyballPBP(unittest.TestCase):
         self.client = app.test_client()
 
     def test_uprm_roster_remove_valid(self):
-        response = self.client.delete('/pbp/Voleibol/roster', data=json.dumps(
-            data["valid_to_remove"]), content_type='application/json', follow_redirects=True)
+        response = self.client.delete('/pbp/Voleibol/roster?event_id=28&athlete_id=115&team=uprm', data=json.dumps(
+            {}), content_type='application/json', follow_redirects=True)
         expected_msg = "La información del atleta se ha removido del sistema."
         self.assertEqual(response.status_code, 200)
         self.assertMultiLineEqual(expected_msg, response.json["MSG"])
