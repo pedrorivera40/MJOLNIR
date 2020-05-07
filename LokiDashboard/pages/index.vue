774eb53a8d4318e6244309ffaafe0f201b645283
Onix Tarrats Calderon <onix.tarrats@upr.edu>
2020-05-07 14:04:48 -0400

    Fixed nvaigation bug on home screen and colors of the gradient

--- LokiDashboard/pages/index.vue
+++ LokiDashboard/pages/index.vue
@@ -1,11 +1,13 @@
 <template>
-  <v-layout column class="wrapper">
+  <v-layout v-if="visible" column class="wrapper">
     <v-flex xs12 sm8 md6>
       <div class="text-center">
-        <h1 class="header my-12" dark>Welcome to Huella Deportiva Dashboard!</h1>
+        <h1 class="header my-12" dark>
+          Welcome to Huella Deportiva Dashboard!
+        </h1>
         <logo />
-        <h2 class="my-6" >Please, proceed to the login page.</h2>
-        <v-btn color="primary_dark" dark link to="/login">
+        <h2 class="my-6">Please, proceed to the login page.</h2>
+        <v-btn color="primary_dark" dark @click="proceed">
           Continue
         </v-btn>
       </div>
@@ -22,13 +24,30 @@ export default {
   layout: "guest",
   components: {
     Logo
+  },
+  data() {
+    return {
+      visible: true,
+    };
+  },
+  methods: {
+    proceed() {
+      this.visible = false
+      return this.$router.push("/login");
+    }
   }
 };
 </script>
 
 <style lang="scss" scoped>
 .wrapper {
-  background: linear-gradient(-45deg, rgb(55, 141, 62),rgb(129, 185, 129), rgb(55, 141, 66), rgb(5, 36, 4));
+  background: linear-gradient(
+    -45deg,
+    rgb(55, 141, 62),
+    rgb(72, 165, 72),
+    rgb(55, 141, 66),
+    rgb(5, 36, 4)
+  );
   background-size: 400% 400%;
   color: whitesmoke;
   -webkit-animation: Gradient 35s ease infinite;
@@ -48,11 +67,17 @@ export default {
   0% {
     background-position: 0% 50%;
   }
-  50%{
-  background-position: 100% 50%;
+  25% {
+    background-position: 50% 50%;
   }
-  100%{
-  background-position: 0% 50%;
+  50% {
+    background-position: 100% 50%;
+  }
+  75% {
+    background-position: 50% 50%;
+  }
+  100% {
+    background-position: 0% 50%;
   }
 }
 </style>
