ac401446d6dd7da4b564633b05370d59a942e95a
Onix Tarrats Calderon <onix.tarrats@upr.edu>
2020-05-07 13:12:40 -0400

    Added animation to the homescreen

--- LokiDashboard/pages/index.vue
+++ LokiDashboard/pages/index.vue
@@ -1,16 +1,10 @@
 <template>
-  <v-layout
-    column
-  >
-    <v-flex
-      xs12
-      sm8
-      md6
-    >
+  <v-layout column class="wrapper">
+    <v-flex xs12 sm8 md6>
       <div class="text-center">
-        <h1 class="header my-12">Welcome to Huella Deportiva Dashboard!</h1>
+        <h1 class="header my-12" dark>Welcome to Huella Deportiva Dashboard!</h1>
         <logo />
-        <h2 class="my-6">Please, proceed to the login page.</h2>
+        <h2 class="my-6" >Please, proceed to the login page.</h2>
         <v-btn color="primary_dark" dark link to="/login">
           Continue
         </v-btn>
@@ -20,20 +14,45 @@
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
+import Logo from "~/components/Logo.vue";
 
 export default {
   auth: false,
-  middleware: 'guest',
-  layout: 'guest',
+  middleware: "guest",
+  layout: "guest",
   components: {
-    Logo,
+    Logo
   }
-}
+};
 </script>
 
 <style lang="scss" scoped>
+.wrapper {
+  background: linear-gradient(-45deg, rgb(55, 141, 62),rgb(129, 185, 129), rgb(55, 141, 66), rgb(5, 36, 4));
+  background-size: 400% 400%;
+  color: whitesmoke;
+  -webkit-animation: Gradient 35s ease infinite;
+  -moz-animation: Gradient 35s ease infinite;
+  animation: Gradient 35s ease infinite;
+  height: 100vh;
+  position: absolute;
+  top: 0;
+  left: 0;
+  color: gree;
+  width: 100vw;
   .header {
     font-size: 3rem;
   }
+}
+@keyframes Gradient {
+  0% {
+    background-position: 0% 50%;
+  }
+  50%{
+  background-position: 100% 50%;
+  }
+  100%{
+  background-position: 0% 50%;
+  }
+}
 </style>
