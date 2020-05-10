7fbb1b265db5aa4d7f3851c27feb453d79137784
herbert-perez <herbert.perez@upr.edu>
2020-05-10 01:30:15 -0400

    added loadingQuery to the website/Results, stopped Website from showing message of not existing content when its still loading

--- FreyaWebsite/store/results/actions.js
+++ FreyaWebsite/store/results/actions.js
@@ -9,10 +9,12 @@ export default{
             //console.log("GET ALL EVENT STATS",response)
             console.log("GET ALL EVENT STATS",response.data)
             commit("SET_RESULTS_PAYLOAD",response.data)
+            commit("SET_QUERY_DONE")
             
         }catch(error){
             console.log("ERROR GETTING ALL EVENT STATS",stat_params,error)
             commit("SET_RESULTS_PAYLOAD",null)
+            commit("SET_QUERY_DONE")
         }
     },
     async getEventInfo({commit},event_id){
@@ -22,10 +24,12 @@ export default{
             //console.log("GET EVENT INFO",response)
             console.log("GET EVENT INFO",response.data)
             commit("SET_EVENT_INFO",response.data.Event)
+            // commit("SET_QUERY_DONE")
             
         }catch(error){
             console.log("ERROR GETTING EVENT INFO",event_id,error)
             commit("SET_EVENT_INFO",null)
+            commit("SET_QUERY_DONE")
         }
     },
     async clearAllStats({commit}){
@@ -40,6 +44,15 @@ export default{
             commit("SET_EVENT_INFO",null)
         }catch(error){
             console.log("ERROR SETTING EVENT INFO TO NULL",error)
+        }
+    },
+    //NEW ACTIONS FOR THE DASHBOARD INTEGRATION
+    //GENERAL USE
+    async setQueryLoading({commit}){
+        try{
+            commit("SET_QUERY_LOADING")
+        }catch(error){
+            console.log("ERROR SETTING STATE VARIABLE FOR LOADING QUERY",error)
         }
     },
 }
