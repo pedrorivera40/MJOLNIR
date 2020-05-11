6cbd6b8bc39b8624cc09c3481278f34609bf15f8
Onix Tarrats Calderon <onix.tarrats@upr.edu>
2020-05-11 18:50:34 -0400

    Removed test fucntion from default layout

--- LokiDashboard/layouts/default.vue
+++ LokiDashboard/layouts/default.vue
@@ -121,9 +121,6 @@ export default {
       logout: "userAuth/logout",
       setUser: "userAuth/setUser"
     }),
-    test() {
-      console.log(this.$store.state.userAuth.userPermissions[9]["22"]);
-    }
   },
   computed: {
     ...mapGetters({
