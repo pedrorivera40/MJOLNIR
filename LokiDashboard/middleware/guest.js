ad2a4c145035718cb95e2d58b7bb9ccca7bbc7e3
Onix Tarrats Calderon <onix.tarrats@upr.edu>
2020-05-12 01:27:14 -0400

    Experimental problem fix

--- LokiDashboard/middleware/guest.js
+++ LokiDashboard/middleware/guest.js
@@ -1,5 +1,14 @@
-export default function ({$auth, redirect}){
-  if($auth.loggedIn){
-    return redirect('/dashboard')
-  }
+const cookieparser = process.server ? require('cookieparser') : undefined
+
+export default function ({ $auth, redirect, req }) {
+  if (req.headers.cookie) {
+    const parsed = cookieparser.parse(req.headers.cookie)
+    console.log(parsed)
+
+    if ($auth.loggedIn) {
+      if (parsed.user)
+      console.log('user cookie',parsed.user)
+        return redirect('/dashboard')
+    }
+  } else return redirect('/login')
 }
