0e307f005cb33c56adcf24dd28ef4461a4da5d7b
Onix Tarrats Calderon <onix.tarrats@upr.edu>
2020-05-13 14:16:28 -0400

    Aligned filter button to the right of the screen

--- FreyaWebsite/pages/eventos/index.vue
+++ FreyaWebsite/pages/eventos/index.vue
@@ -4,63 +4,95 @@
       <h1>Eventos:</h1>
     </v-row>
 
-    <v-menu
-      v-model="menu"
-      bottom
-      origin="center center"
-      transition="slide-x-transition"
-      :close-on-content-click="false"
-    >
-      <template v-slot:activator="{ on }">        
-        <v-btn color="green darken-1" dark v-on="on">
-          <v-icon left>mdi-filter-variant</v-icon>Filtros
-        </v-btn>
-      </template>
+    <v-row class="text-right">
+      <v-col align="end">
+        <v-menu
+          v-model="menu"
+          bottom
+          origin="center center"
+          transition="slide-x-transition"
+          :close-on-content-click="false"
+        >
+          <template v-slot:activator="{ on }">
+            <v-btn color="green darken-1" dark v-on="on">
+              <v-icon left>mdi-filter-variant</v-icon>Filtros
+            </v-btn>
+          </template>
 
-      <v-list>
-        <v-list-item>
-          <v-menu
-            ref="menu"
-            v-model="dateMenu"
-            :close-on-content-click="false"
-            :return-value.sync="date"
-            transition="scale-transition"
-            offset-y
-            min-width="290px"
-          >
-            <template v-slot:activator="{ on }">
-              <v-text-field v-model="date" label="Fecha del Evento" readonly v-on="on"></v-text-field>
-            </template>
-            <v-date-picker v-model="date" color="green darken-1" no-title scrollable locale="es-419">
-              <v-spacer></v-spacer>
-              <v-btn text color="primary" @click="dateMenu = false">Cancelar</v-btn>
-              <v-btn text color="primary" @click="$refs.menu.save(date)">OK</v-btn>
-            </v-date-picker>
-          </v-menu>
-        </v-list-item>
-        <v-list-item>
-          <v-autocomplete v-model="sport" :items="sports" label="Deporte"></v-autocomplete>
-        </v-list-item>
-        <v-list-item>
-          <v-select v-model="branch" :items="branches" label="Rama"></v-select>
-        </v-list-item>
-        <v-list-item>
-          <v-select v-model="locality" :items="localities" label="Localizacion"></v-select>
-        </v-list-item>
-        <v-list-item>
-          <v-checkbox v-model="eventHasPBP" label="Eventos con PBP"> </v-checkbox>
-        </v-list-item>
+          <v-list>
+            <v-list-item>
+              <v-menu
+                ref="menu"
+                v-model="dateMenu"
+                :close-on-content-click="false"
+                :return-value.sync="date"
+                transition="scale-transition"
+                offset-y
+                min-width="290px"
+              >
+                <template v-slot:activator="{ on }">
+                  <v-text-field
+                    v-model="date"
+                    label="Fecha del Evento"
+                    readonly
+                    v-on="on"
+                  ></v-text-field>
+                </template>
+                <v-date-picker
+                  v-model="date"
+                  color="green darken-1"
+                  no-title
+                  scrollable
+                  locale="es-419"
+                >
+                  <v-spacer></v-spacer>
+                  <v-btn text color="primary" @click="dateMenu = false"
+                    >Cancelar</v-btn
+                  >
+                  <v-btn text color="primary" @click="$refs.menu.save(date)"
+                    >OK</v-btn
+                  >
+                </v-date-picker>
+              </v-menu>
+            </v-list-item>
+            <v-list-item>
+              <v-autocomplete
+                v-model="sport"
+                :items="sports"
+                label="Deporte"
+              ></v-autocomplete>
+            </v-list-item>
+            <v-list-item>
+              <v-select
+                v-model="branch"
+                :items="branches"
+                label="Rama"
+              ></v-select>
+            </v-list-item>
+            <v-list-item>
+              <v-select
+                v-model="locality"
+                :items="localities"
+                label="Localizacion"
+              ></v-select>
+            </v-list-item>
+            <v-list-item>
+              <v-checkbox v-model="eventHasPBP" label="Eventos con PBP">
+              </v-checkbox>
+            </v-list-item>
 
-        <v-list-item>
-          <v-btn @click="clearFilters">Borrar</v-btn>
-          <v-spacer />
-          <v-btn @click="createFilteredList">Filtrar</v-btn>
-        </v-list-item>
-      </v-list>
-    </v-menu>
+            <v-list-item>
+              <v-btn @click="clearFilters">Borrar</v-btn>
+              <v-spacer />
+              <v-btn @click="createFilteredList">Filtrar</v-btn>
+            </v-list-item>
+          </v-list>
+        </v-menu>
+      </v-col>
+    </v-row>
 
-    <v-row v-if="filteredEvents!=''">
-      <v-col v-for="(value,key) in filteredEvents" :key="key" md="3">
+    <v-row v-if="filteredEvents != ''">
+      <v-col v-for="(value, key) in filteredEvents" :key="key" md="3">
         <EventCard
           :eventID="value.id"
           :sportName="value.sport_name"
@@ -76,7 +108,7 @@
     </v-row>
 
     <v-row v-else>
-      <v-col v-for="(value,key) in events" :key="key" md="3">
+      <v-col v-for="(value, key) in events" :key="key" md="3">
         <EventCard
           :eventID="value.id"
           :sportName="value.sport_name"
@@ -90,7 +122,6 @@
         />
       </v-col>
     </v-row>
-
   </v-container>
 </template>
 
@@ -125,22 +156,21 @@ export default {
       "Campo Traviesa",
       "Halterofilia",
       "Taekwondo",
-      "Porrismo",
-
+      "Porrismo"
     ],
     branches: ["Masculino", "Femenino", "Exhibición"],
     localities: ["Casa", "Afuera"],
-    filteredEvents: '',//This list is the one presented in the events viewer when filtered.
-    eventHasPBP: false,
+    filteredEvents: "", //This list is the one presented in the events viewer when filtered.
+    eventHasPBP: false
   }),
 
   methods: {
     ...mapActions({
       getEvents: "events/getEvents"
-    }),    
+    }),
 
     /**
-     * Clears the filter fields 
+     * Clears the filter fields
      * and resets the filtered list
      * if filters have been applied.
      */
@@ -150,8 +180,8 @@ export default {
       this.branch = "";
       this.locality = "";
       this.eventHasPBP = false;
-      this.menu = false;      
-      
+      this.menu = false;
+
       if (this.filteredEvents.length != this.events.length) {
         this.filteredEvents = [];
         for (let i = 0; i < this.events.length; i++) {
@@ -159,18 +189,15 @@ export default {
         }
       }
     },
-    
-     
-    
+
     /**
-     * Creates a the filtered list, after 
+     * Creates a the filtered list, after
      * the user has clicked to filte button,
      * using the the filters selected.
      */
     createFilteredList() {
-      
       this.filteredEvents = [];
-      
+
       for (let i = 0; i < this.events.length; i++) {
         this.filteredEvents.push(this.events[i]);
       }
@@ -207,29 +234,25 @@ export default {
             continue;
           }
         }
-        if(this.eventHasPBP == true){
-          if(!event.hasPBP){
-            this.filteredEvents.splice(i,1);
-            continue
+        if (this.eventHasPBP == true) {
+          if (!event.hasPBP) {
+            this.filteredEvents.splice(i, 1);
+            continue;
           }
         }
       }
-      this.menu = false
+      this.menu = false;
     }
   },
 
   computed: {
     ...mapGetters({
       events: "events/events"
-    }),
-    
-    
+    })
   },
-  
+
   mounted() {
     this.getEvents();
-    
-    
   }
-}
+};
 </script>
