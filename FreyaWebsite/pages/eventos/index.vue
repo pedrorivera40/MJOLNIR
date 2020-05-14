712ac551e271111f3d297ab5c1899a260e625783
Onix Tarrats Calderon <onix.tarrats@upr.edu>
2020-05-14 09:46:51 -0400

    Added loader to events

--- FreyaWebsite/pages/eventos/index.vue
+++ FreyaWebsite/pages/eventos/index.vue
@@ -3,139 +3,152 @@
     <v-row>
       <h1>Eventos:</h1>
     </v-row>
+ 
+    <div v-if="events.length>0">
+      <v-row class="text-right">
+        <v-col align="end">
+          <v-menu
+            v-model="menu"
+            bottom
+            origin="center center"
+            transition="slide-x-transition"
+            :close-on-content-click="false"
+          >
+            <template v-slot:activator="{ on }">
+              <v-btn color="green darken-1" dark v-on="on">
+                <v-icon left>mdi-filter-variant</v-icon>Filtros
+              </v-btn>
+            </template>
 
-    <v-row class="text-right">
-      <v-col align="end">
-        <v-menu
-          v-model="menu"
-          bottom
-          origin="center center"
-          transition="slide-x-transition"
-          :close-on-content-click="false"
-        >
-          <template v-slot:activator="{ on }">
-            <v-btn color="green darken-1" dark v-on="on">
-              <v-icon left>mdi-filter-variant</v-icon>Filtros
-            </v-btn>
-          </template>
-
-          <v-list>
-            <v-list-item>
-              <v-menu
-                ref="menu"
-                v-model="dateMenu"
-                :close-on-content-click="false"
-                :return-value.sync="date"
-                transition="scale-transition"
-                offset-y
-                min-width="290px"
-              >
-                <template v-slot:activator="{ on }">
-                  <v-text-field
-                    v-model="date"
-                    label="Fecha del Evento"
-                    readonly
-                    v-on="on"
-                  ></v-text-field>
-                </template>
-                <v-date-picker
-                  v-model="date"
-                  color="green darken-1"
-                  no-title
-                  scrollable
-                  locale="es-419"
+            <v-list>
+              <v-list-item>
+                <v-menu
+                  ref="menu"
+                  v-model="dateMenu"
+                  :close-on-content-click="false"
+                  :return-value.sync="date"
+                  transition="scale-transition"
+                  offset-y
+                  min-width="290px"
                 >
-                  <v-spacer></v-spacer>
-                  <v-btn text color="primary" @click="dateMenu = false"
-                    >Cancelar</v-btn
+                  <template v-slot:activator="{ on }">
+                    <v-text-field
+                      v-model="date"
+                      label="Fecha del Evento"
+                      readonly
+                      v-on="on"
+                    ></v-text-field>
+                  </template>
+                  <v-date-picker
+                    v-model="date"
+                    color="green darken-1"
+                    no-title
+                    scrollable
+                    locale="es-419"
                   >
-                  <v-btn text color="primary" @click="$refs.menu.save(date)"
-                    >OK</v-btn
-                  >
-                </v-date-picker>
-              </v-menu>
-            </v-list-item>
-            <v-list-item>
-              <v-autocomplete
-                v-model="sport"
-                :items="sports"
-                label="Deporte"
-              ></v-autocomplete>
-            </v-list-item>
-            <v-list-item>
-              <v-select
-                v-model="branch"
-                :items="branches"
-                label="Rama"
-              ></v-select>
-            </v-list-item>
-            <v-list-item>
-              <v-select
-                v-model="locality"
-                :items="localities"
-                label="Localizacion"
-              ></v-select>
-            </v-list-item>
-            <v-list-item>
-              <v-checkbox v-model="eventHasPBP" label="Eventos con PBP">
-              </v-checkbox>
-            </v-list-item>
+                    <v-spacer></v-spacer>
+                    <v-btn text color="primary" @click="dateMenu = false"
+                      >Cancelar</v-btn
+                    >
+                    <v-btn text color="primary" @click="$refs.menu.save(date)"
+                      >OK</v-btn
+                    >
+                  </v-date-picker>
+                </v-menu>
+              </v-list-item>
+              <v-list-item>
+                <v-autocomplete
+                  v-model="sport"
+                  :items="sports"
+                  label="Deporte"
+                ></v-autocomplete>
+              </v-list-item>
+              <v-list-item>
+                <v-select
+                  v-model="branch"
+                  :items="branches"
+                  label="Rama"
+                ></v-select>
+              </v-list-item>
+              <v-list-item>
+                <v-select
+                  v-model="locality"
+                  :items="localities"
+                  label="Localizacion"
+                ></v-select>
+              </v-list-item>
+              <v-list-item>
+                <v-checkbox v-model="eventHasPBP" label="Eventos con PBP">
+                </v-checkbox>
+              </v-list-item>
 
-            <v-list-item>
-              <v-btn @click="clearFilters">Borrar</v-btn>
-              <v-spacer />
-              <v-btn @click="createFilteredList">Filtrar</v-btn>
-            </v-list-item>
-          </v-list>
-        </v-menu>
-      </v-col>
-    </v-row>
+              <v-list-item>
+                <v-btn @click="clearFilters">Borrar</v-btn>
+                <v-spacer />
+                <v-btn @click="createFilteredList">Filtrar</v-btn>
+              </v-list-item>
+            </v-list>
+          </v-menu>
+        </v-col>
+      </v-row>
 
-    <v-row v-if="filteredEvents != ''">
-      <v-col v-for="(value, key) in filteredEvents" :key="key" cols="12"
-        sm="6"
-        lg="4">
-        <EventCard
-          :eventID="value.id"
-          :sportName="value.sport_name"
-          :eventDate="value.event_date"
-          :opponentName="value.opponent_name"
-          :img="value.sport_img_url"
-          :localScore="value.local_score"
-          :opponentScore="value.opponent_score"
-          :eventSummary="value.event_summary"
-          :hasPBP="value.hasPBP"
-        />
-      </v-col>
-    </v-row>
+      <v-row v-if="!!filteredEvents">
+        <v-col
+          v-for="(value, key) in filteredEvents"
+          :key="key"
+          cols="12"
+          sm="6"
+          lg="4"
+        >
+          <EventCard
+            :eventID="value.id"
+            :sportName="value.sport_name"
+            :eventDate="value.event_date"
+            :opponentName="value.opponent_name"
+            :img="value.sport_img_url"
+            :localScore="value.local_score"
+            :opponentScore="value.opponent_score"
+            :eventSummary="value.event_summary"
+            :hasPBP="value.hasPBP"
+          />
+        </v-col>
+      </v-row>
 
-    <v-row v-else>
-      <v-col v-for="(value, key) in events" :key="key" cols="12"
-        sm="6"
-        lg="4">
-        <EventCard
-          :eventID="value.id"
-          :sportName="value.sport_name"
-          :eventDate="value.event_date"
-          :opponentName="value.opponent_name"
-          :img="value.sport_img_url"
-          :localScore="value.local_score"
-          :opponentScore="value.opponent_score"
-          :eventSummary="value.event_summary"
-          :hasPBP="value.hasPBP"
-        />
-      </v-col>
-    </v-row>
+      <v-row v-else>
+        <v-col
+          v-for="(value, key) in events"
+          :key="key"
+          cols="12"
+          sm="6"
+          lg="4"
+        >
+          <EventCard
+            :eventID="value.id"
+            :sportName="value.sport_name"
+            :eventDate="value.event_date"
+            :opponentName="value.opponent_name"
+            :img="value.sport_img_url"
+            :localScore="value.local_score"
+            :opponentScore="value.opponent_score"
+            :eventSummary="value.event_summary"
+            :hasPBP="value.hasPBP"
+          />
+        </v-col>
+      </v-row>
+    </div>
+    <LoadingScreen v-else name-of-data="Eventos" />
   </v-container>
 </template>
 
 <script>
 import { mapActions, mapGetters } from "vuex";
 import EventCard from "../../components/EventCard";
+import LoadingScreen from "../../components/general/LoadingScreen";
 
 export default {
   components: {
-    EventCard
+    EventCard,
+    LoadingScreen
   },
 
   data: () => ({
