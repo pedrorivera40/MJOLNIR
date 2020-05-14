35d34a49dcd8d1f7e34ff9ec99a334e0bc104863
pedrorivera40 <pedro.rivera40@upr.edu>
2020-05-14 14:00:11 -0400

    updates in freya & loki (making them uniform, loaders, and error card)

---
+++ LokiDashboard/components/PBP/ErrorCard.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-card>
+    <v-toolbar :color="in_color" dark flat>
+      <v-row justify="center" align="center">
+        <v-card-title class="title text-center" style="word-break: normal;">{{ error_header }}</v-card-title>
+      </v-row>
+    </v-toolbar>
+    <v-row>
+      <v-card-text class="subtitle-1 text-center" style="word-break: normal;">{{ error_message }}</v-card-text>
+    </v-row>
+  </v-card>
+</template>
+
+<script>
+export default {
+  props: {
+    error_header: String,
+    error_message: String,
+    in_color: String
+  }
+};
+</script>
+
